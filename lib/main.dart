7e4b7f54f734a25f72dcdee9e191df2d14229bd1
Kayson3 <Ayomiposiibikunle@gmail.com>
2021-07-03 23:25:33 +0100

    u

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart';
@@ -39,6 +40,33 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Column(children: [
           Container(
             height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.purple,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Text('Currently in Abuja',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14.0))),
+                Text("52\u00B0",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600)),
+                Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text('Rain',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14.0))),
+              ],
+            ),
           )
         ]));
   }
