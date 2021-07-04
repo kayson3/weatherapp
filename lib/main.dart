6b7a6a10b239584a3b7d70f55443381693cbf0d8
Kayson3 <Ayomiposiibikunle@gmail.com>
2021-07-04 08:28:17 +0100

    s

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
+
+//http://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=2beb83c72a7edba1158a6b1cc981f660
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -31,6 +34,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var temp;
+  var description;
+  var currently;
+  var humidity;
+  var windspeed;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,24 +59,51 @@ class _MyHomePageState extends State<MyHomePage> {
                     padding: EdgeInsets.only(bottom: 10),
                     child: Text('Currently in Abuja',
                         style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 14.0))),
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0,
+                        ))),
                 Text("52\u00B0",
                     style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600)),
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    )),
                 Padding(
                     padding: EdgeInsets.only(top: 10),
                     child: Text('Rain',
                         style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 14.0))),
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0,
+                        ))),
               ],
             ),
-          )
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                    title: Text("Temperature"),
+                    trailing: Text("52\u00B0")),
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text("Weather"),
+                    trailing: Text("weather")),
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text("Humidity"),
+                    trailing: Text("12")),
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text("WindSpeed"),
+                    trailing: Text("12"))
+              ],
+            ),
+          ))
         ]));
   }
 }
